82072cb4677fd409b33f91ac2303a79faaf266bb
tobiakinlade <tobi.akinlade@yahoo.com>
2022-05-08 15:36:05 +0100

    Posting image to firebase implemented

--- components/Modal.js
+++ components/Modal.js
@@ -3,12 +3,53 @@ import { modalState } from '../atoms/modalAtom';
 import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, useState, useRef } from 'react';
 import { CameraIcon } from '@heroicons/react/outline';
+import { db, storage } from '../firebase';
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  updateDoc,
+  doc,
+} from 'firebase/firestore';
+import { useSession } from 'next-auth/react';
+import { ref, getDownloadURL, uploadString } from '@firebase/storage';
 
 function Modal() {
+  const { data: session } = useSession();
   const [open, setOpen] = useRecoilState(modalState);
-  const filePickerRef = useRef();
+  const filePickerRef = useRef(null);
   const [selectedFile, setSelectedFile] = useState(null);
-  const captionRef = useRef();
+  const captionRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+
+  const uploadPost = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    const docRef = await addDoc(collection(db, 'posts'), {
+      username: session.user.username,
+      caption: captionRef.current.value,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+
+    console.log('New doc added with ID', docRef.id);
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+    await uploadString(imageRef, selectedFile, 'data_url').then(
+      async (snapshot) => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, 'posts', docRef.id), {
+          image: downloadURL,
+        });
+      }
+    );
+    setOpen(false);
+    setLoading(false);
+    setSelectedFile(null);
+  };
 
   const addImageToPost = (e) => {
     const reader = new FileReader();
@@ -21,7 +62,6 @@ function Modal() {
     };
   };
 
-  const uploadPost = async () => {};
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -105,11 +145,11 @@ function Modal() {
                 <div className='mt-5 sm:mt-6'>
                   <button
                     type='button'
-                    // disabled={!selectedFile}
+                    disabled={!selectedFile}
                     className='inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300'
-                    // onClick={uploadPost}
+                    onClick={uploadPost}
                   >
-                    Upload Post
+                    {loading ? 'UpLoading...' : 'Upload Post'}
                   </button>
                 </div>
               </div>
